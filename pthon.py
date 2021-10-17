5f5b25f33f75f0ad2a09e9987a792ebf28d1d86b
farazaraf <84454770+farazaraf@users.noreply.github.com>
2021-10-17 12:58:07 +0500

    Update pthon.py

--- pthon.py
+++ pthon.py
@@ -11,37 +11,50 @@ class Stack:
     def peak(self):
         return self.container[-1]
     def stack_checker(self,val):
-        if self.container[-1] == val:
-            is_balanced = True
-            self.pop()                #pop bracket out
-            print(is_balanced)
-        else: 
-            is_balanced = False
-            print(is_balanced)
+        global is_balanced
+        if len(self.container) > 0:
+            if self.container[-1] == val:
+                self.is_balanced = True
+                self.pop()                #pop bracket out
+                return self.is_balanced
+            else: 
+                self.is_balanced = False
+                self.container.clear()
+                return self.is_balanced
+        else: self.is_balanced = False        
         
     def check_brackets(self,string):
         my_string = list(string)
+      
         for x in my_string:
             if x == '(':               #push it in the stack
                 self.push(x)
-            elif x == ')':             #check if bracket at top of stack is the same type
+            elif x == ')':             #check if bracket at top of stack is the same 
                 self.stack_checker('(')
-            if x == '{':
+                if self.is_balanced == False:
+                    break
+            elif x == '{':
                 self.push(x)
             elif x == '}':
                 self.stack_checker('{')
-            if x == '[':
+                if self.is_balanced == False:
+                    break
+            elif x == '[':
                 self.push(x)
             elif x == ']':
                 self.stack_checker('[')
+                if self.is_balanced == False:
+                    break
+
+        return self.is_balanced
 s = Stack()
 user_string = "({a+b})"
 print(user_string, " " , s.check_brackets(user_string))
-user_string = "{()}[]"
-print(user_string," ", s.check_brackets(user_string))
 user_string = "))((a+b}{"
 print(user_string," ", s.check_brackets(user_string))
 user_string = "((a+b))"
 print(user_string," ", s.check_brackets(user_string))
-user_string = "[a+b]*(x+2y)*(gg+kk)" 
+user_string = "))"
+print(user_string," ", s.check_brackets(user_string))
+user_string = "[a+b]*(x+2y)*{gg+kk}" 
 print(user_string," ", s.check_brackets(user_string))
